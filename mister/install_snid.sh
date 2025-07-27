f273b2f952bb03d2896d3c833ea7e978e2d98e05
Jonathan Keller <19418817+NobodyNada@users.noreply.github.com>
2025-07-27 16:39:14 -0700

    add install script

--- mister/install_snid.sh
+++ mister/install_snid.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
+set -e
 
+echo "Downloading manifest..."
+curl https://jonathankeller.net/mister/manifest.json | jq -r '.[] | [.src, .dst] | join(" ")' | while read src dst; do
+    echo "Downloading" $src
+    curl -SL $src -o /tmp/download
+    mv /tmp/download $dst
+done
+
+echo "Done, rebooting..."
+sleep 3
+reboot
