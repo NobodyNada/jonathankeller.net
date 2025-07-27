d2043fdd0716124970b45bb25ef3f9548d1dc834
Jonathan Keller <19418817+NobodyNada@users.noreply.github.com>
2025-07-27 16:54:43 -0700

    use wget instead of curl due to cert issue

--- mister/install_snid.sh
+++ mister/install_snid.sh
@@ -3,9 +3,9 @@
 set -e
 
 echo "Downloading manifest..."
-curl https://jonathankeller.net/mister/manifest.json | jq -r '.[] | [.src, .dst] | join(" ")' | while read src dst; do
+wget -O- https://jonathankeller.net/mister/manifest.json | jq -r '.[] | [.src, .dst] | join(" ")' | while read src dst; do
     echo "Downloading" $src
-    curl -SL $src -o /tmp/download
+    wget -SL $src -O /tmp/download
     mv /tmp/download $dst
 done
 
