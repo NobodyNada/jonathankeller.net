e34750f982829a41d4b0c6bd2191754c4b440c44
Jonathan Keller <19418817+NobodyNada@users.noreply.github.com>
2025-07-27 17:24:58 -0700

    mount filesystem as RW

--- mister/install_snid.sh
+++ mister/install_snid.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-set -e
+set -euo pipefail
+
+mount -o remount,rw /
 
 echo "Downloading manifest..."
 wget -O- https://jonathankeller.net/mister/manifest.json | jq -r '.[] | [.src, .dst] | join(" ")' | while read src dst; do
